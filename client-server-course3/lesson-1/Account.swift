05902be44ded7de0bac5db5b80295d36e4f823c9
Sunsar <sunsar@me.com>
2021-06-22 18:48:20 +0300

    singletone, NsNotifikationCenter

---
+++ client-server-course3/lesson-1/Account.swift
@@ -0,0 +1,19 @@
+//
+//  Account.swift
+//  client-server-course3
+//
+//  Created by suncoda on 22.06.2021.
+//
+
+import Foundation
+
+final class Account {
+    
+    private init() {}
+     
+        static let shared = Account()
+        
+        var name: String = ""
+        var cash: Int = 0
+        
+}
---
+++ client-server-course3/lesson-1/DetailAccountViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailAccountViewController.swift
+//  client-server-course3
+//
+//  Created by suncoda on 22.06.2021.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class DetailAccountViewController: UICollectionViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var cashLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = Account.shared.name
+        cashLabel.text = "\(Account.shared.cash)"
+        
+    }
+    
+    
+}
